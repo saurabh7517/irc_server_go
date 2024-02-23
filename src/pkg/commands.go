3ecbe25c54eac7c022a69fd0c2f2f5fa43868146
Saurabh Kumar <saurabh7517@gmail.com>
2024-02-23 20:07:03 +0000

    database and localcache

--- src/pkg/commands.go
+++ src/pkg/commands.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	user "irc_server/src/objects"
+	"log"
+)
+
 const NICK string = "NICK"
 const USER string = "USER"
 const WHOIS string = "WHOIS"
@@ -48,7 +53,69 @@ func processNick(parameters []string) {
 	//connect to database and find out
 	//if nick name already exist return ERR_NICKNAMEINUSE
 	// if not :bar.example.com(server name) 001(RPL_WELCOME) :<message> <nick>!<user>@<host>
+	// rows, err := db.Query("SELECT * FROM album WHERE artist = ?", artist)
+	ProcessNickMessage()
+	// var db *sql.DB = GetDBConnection()
+	rows, err := db.Query("SELECT nickname from user where nickname=? limit 1", parameters[1])
+	if err != nil {
+		log.Printf("Error retrieving rows from database")
+	}
+	defer rows.Close()
 
+	var foundUser user.User
+	for rows.Next() {
+		err := rows.Scan(&foundUser.Id, &foundUser.Nickname, &foundUser.Name)
+		if err != nil {
+			log.Printf(err.Error())
+			//return appropriate error to client
+		}
+		//user found in database
+		//store this user in server cache
+		//let every server know about it, and donot allow any user connection with this nickname
+	}
+
+}
+
+func ProcessNickMessage(parameters []string) {
+	//check if nick exists in cache
+	//yes -> user already exists with a connection on server
+	//no -> check if user exists in database
+	//no -> no -> user is not registered
+	//yes -> add user to cache send a successful response
+	if IsUserOnServer(parameters[1]) {
+		//return message "Nickname already in use"
+	} else {
+		if IsUserRegistered(parameters[1]) {
+			//add user to cache
+			AddLogedInUser(parameters[1])
+			// send 001 RPL_WELCOME "Welcome to the Internet Relay Network<nick>!<user>@<host>"
+		} else {
+			//the user trying to connect is not registered, contact admin@admin.com for registration
+		}
+	}
+}
+
+func IsUserRegistered(usr string) bool {
+	// var db *sql.DB = GetDBConnection()
+	rows, err := db.Query("SELECT nickname from user where nickname=? limit 1", usr)
+	if err != nil {
+		log.Printf("Error retrieving rows from database")
+	}
+	defer rows.Close()
+
+	var found bool = false
+
+	var foundUser user.User
+	for rows.Next() {
+		err := rows.Scan(&foundUser.Id, &foundUser.Nickname, &foundUser.Name)
+		if err != nil {
+			log.Printf(err.Error())
+			//return appropriate error to client
+		}
+		found = true
+		break
+	}
+	return found
 }
 
 func processUser() {
